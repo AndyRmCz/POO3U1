8382289588bed44a3aa174ff3565091a97433a6b
Andres Ramos <andres_gerardo_ramos@bat.com>
2023-04-17 12:24:41 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame();
+        frame.setSize(420,420);
+        frame.setVisibe = True;
+    }
+}
